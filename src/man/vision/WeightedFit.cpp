722eb191a02d0dc7dd9a659df17332b3b667bab4
Josh Imhoff <joshimhoff13@gmail.com>
2015-01-31 19:41:37 -0500

    Completes everything but ssError in WeightedFit class

--- src/man/vision/WeightedFit.cpp
+++ src/man/vision/WeightedFit.cpp
@@ -1,25 +1,17 @@
-#include <math.h>
 #include "WeightedFit.h"
 
 namespace man {
 namespace vision {
 
-double WeightedFit::angle()
+double WeightedFit::clear()
 {
-    solve();
-    return atan2(mxy, mx - my) / 2;
-}
-
-double WeightedFit::principalLength1()
-{
-    solve();
-    return sqrt((mx + my + len) / 2) / sw;
-}
-
-double WeightedFit::principalLength2()
-{
-    solve();
-    return sqrt((mx + my - len) / 2) / sw;
+    sw = 0;
+    sx = 0;
+    sy = 0;
+    sx2 = 0;
+    sy2 = 0;
+    sxy = 0;
+    solved = false;
 }
 
 void WeightedFit::solve()
@@ -27,10 +19,24 @@ void WeightedFit::solve()
     if (solved) 
         return;
 
-    mx = sw * sx2 - sx * sx;
-    my = sw * sy2 - sy * sy;
-    mxy = 2 * (sw * sxy - sx * sy);
-    len = sqrt(mxy * mxy + (mx - my) * (mx - my));
+    double a = sw*sx2 - sx*sx;
+    double b = sw*sy2 - sy*sy;
+    double c = 2.0 * (sw*sxy - sx*sy);
+    double d = sqrt(c*c + (a - b)*(a - b));
+
+    pLen1 = sqrt(1.5*(a + b + d)) / sw;
+    pLen2 = sqrt(1.5*(a + b - d)) / sw;
+
+    if (!d) {
+        uPAI = 0.0;
+        vPai = 0.0;
+    } else {
+        uPAI = sqrt(((a - b)/d + 1) / 2);
+        vPAI = sqrt((1 - (a - b)/d) / 2);
+        if (c < 0.0)
+            uPAI = -uPAI;
+    }
+
     solved = true;
 }
 
